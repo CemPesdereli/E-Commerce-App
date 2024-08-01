853c462638eeabc61a7ea7048a613d7f994a7a2e
CemPesdereli <cempesdereli@gmail.com>
2024-08-01 23:49:24 +0300

    product mapper added, purchaseProducts method added

--- services/product/src/main/java/com/cem/ecommerce/product/ProductPurchaseResponse.java
+++ services/product/src/main/java/com/cem/ecommerce/product/ProductPurchaseResponse.java
@@ -1,6 +1,13 @@
 package com.cem.ecommerce.product;
 
+import java.math.BigDecimal;
+
 public record ProductPurchaseResponse(
 
+        Integer productId,
+        String name,
+        String description,
+        BigDecimal price,
+        double quantity
 ) {
 }
--- services/product/src/main/java/com/cem/ecommerce/product/ProductRepository.java
+++ services/product/src/main/java/com/cem/ecommerce/product/ProductRepository.java
@@ -2,5 +2,10 @@ package com.cem.ecommerce.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product,Integer> {
+    List<Product> findAllByIdInOrderById(List<Integer> productIds);
+
+
 }
