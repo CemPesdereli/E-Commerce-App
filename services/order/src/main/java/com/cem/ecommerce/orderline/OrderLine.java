d85908cab31f673711b8e479ce29a2ed655911d2
CemPesdereli <cempesdereli@gmail.com>
2024-08-02 18:19:44 +0300

    order and orderline entities added

---
+++ services/order/src/main/java/com/cem/ecommerce/orderline/OrderLine.java
@@ -0,0 +1,29 @@
+package com.cem.ecommerce.orderline;
+
+import com.cem.ecommerce.order.Order;
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class OrderLine {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private Integer productId;
+
+    private double quantity;
+
+
+
+}
